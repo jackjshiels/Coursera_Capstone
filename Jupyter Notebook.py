915f46bc4a2ba506b4795b4cfece618e476a6a2c
jackjshiels <73280859+jackjshiels@users.noreply.github.com>
2021-02-04 12:22:58 +0000

    Add files via upload

---
+++ Jupyter Notebook.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# this will be used for capstone
+
+# In[7]:
+
+
+import pandas as pd
+from bokeh.plotting import figure, output_file, show,output_notebook
+output_notebook()
+
+
+# In[8]:
+
+
+import numpy as np
+
+
+# In[13]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
